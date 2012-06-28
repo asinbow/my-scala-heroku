8342e197959c54b1828a341258d4b9e878c0d3df
asinbow.wang <asinbow.wang@saybot.com>
2012-06-28 19:15:41 +0800

    add postgresql support

--- project/Build.scala
+++ project/Build.scala
@@ -4,11 +4,12 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "helloworld"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "heroku"
+    val appVersion      = "0.1.0-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "postgresql" % "postgresql" % "9.1-901.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(

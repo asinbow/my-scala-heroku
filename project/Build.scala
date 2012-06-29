1a4f63bf56d98da92c23297ff3b0caaa30073d8e
asinbow.wang <asinbow.wang@saybot.com>
2012-06-29 16:05:41 +0800

    fix play database url

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,8 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "postgresql" % "postgresql" % "9.1-901.jdbc4"
+      "postgresql" % "postgresql" % "9.1-901.jdbc4",
+      "org.squeryl" %% "squeryl" % "0.9.5-2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
